b3855785a626c55201cb6c0a64698219227c401e
Fizh <abdullahhafizhzahid3@gmail.com>
2025-10-31 18:12:58 +0800

    Add files via upload

---
+++ belajar javascript/script.js
@@ -0,0 +1,18 @@
+let length = prompt('Masukkan luas persegi panjang');
+let width = prompt('Masukkan lebar persegi panjang');
+let area = length * width
+alert("Luas persegi panjang = " + area + " cm2");
+
+var name = "hafizh"
+name = "Azzam"  // bisa diubah
+var name = "hafizh" // bisa di deskripsikan ulang
+console.log(name)
+
+let address = "Bandung"
+address = "Jakarta" // bisa diubah
+// let address = "Bandung" // tidak bisa di deskripsikan ulang
+console.log(address)
+
+const pi = 3.14
+// pi = 2 // tidak bisa diubah dan deskripsikan ulang
+console.log(pi)
